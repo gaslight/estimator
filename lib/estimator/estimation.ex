74f68cda0760fa4e5806a76f2de1fd39109e998f
Chris Nelson <superchrisnelson@gmail.com>
2016-03-01 19:09:36 -0500

    adding estimates underway

--- lib/estimator/estimation.ex
+++ lib/estimator/estimation.ex
@@ -1,5 +1,8 @@
 defmodule Estimator.Estimation do
-  defstruct state: :waiting_for_players, joined_team_members: MapSet.new, story: nil
+  defstruct state: :waiting_for_players,
+            joined_team_members: MapSet.new,
+            story: nil,
+            estimates: []
 
   alias Estimator.Estimation
 
@@ -8,9 +11,22 @@ defmodule Estimator.Estimation do
     |> calculate_state
   end
 
-  defp calculate_state(estimation) do
+  def estimate(estimation, team_member, estimate) do
+    %{ estimation | estimates: [ estimate | estimation.estimates ] }
+    |> calculate_state
+  end
+
+  defp calculate_state(estimation = %Estimation{state: :waiting_for_players}) do
     if estimation.joined_team_members == MapSet.new(estimation.story.project.team_members) do
       %{ estimation | state: :estimating }
+    else
+      estimation
+    end
+  end
+
+  defp calculate_state(estimation = %Estimation{state: :estimating, estimates: estimates}) do
+    if Enum.count(estimates) == Enum.count(estimation.story.project.team_members) do
+      %{ estimation | state: :disagree }
     else
       estimation
     end
--- test/lib/estimation_test.exs
+++ test/lib/estimation_test.exs
@@ -26,7 +26,25 @@ defmodule EstimationTest do
     assert estimation.state == :estimating
   end
 
-  test "adding one estimate" do
+  test "adding one estimate", %{story: story, project: project, estimation: estimation} do
+    [team_member1, team_member2] = project.team_members
+    estimation = estimation
+    |> Estimation.join_estimation(team_member1)
+    |> Estimation.join_estimation(team_member2)
+    |> Estimation.estimate(team_member1, 3)
+    assert estimation.state == :estimating
+    assert Enum.count(estimation.estimates) == 1
+  end
+
+  test "adding two estimate", %{story: story, project: project, estimation: estimation} do
+    [team_member1, team_member2] = project.team_members
+    estimation = estimation
+    |> Estimation.join_estimation(team_member1)
+    |> Estimation.join_estimation(team_member2)
+    |> Estimation.estimate(team_member1, 3)
+    |> Estimation.estimate(team_member2, 2)
+    assert estimation.state == :disagree
+    assert Enum.count(estimation.estimates) == 2
   end
 
 end
